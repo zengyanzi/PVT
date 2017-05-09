85a6408fba43609ebd3ed664d1fd43ceb21a4a52
Jenny <zengzhen.ucf@gmail.com>
2017-05-09 12:04:39 +1200

    gymdetail ui

--- pt/trainee/gymdetail.js
+++ pt/trainee/gymdetail.js
@@ -34,84 +34,81 @@ BackAndroid.addEventListener('hardwareBackPress', function() {
 var _navigator ;
 var btnsDefault = [ { text: 'Button' } ];
 //delete choose item
-detailrows = [
-    {
-      Calories :"457",
-      text:"Rower Moderate  5 min 30 sec fast:60 sec slow",  
-    }, {
-      Calories :"457",
-      text: "Walking Weighted Lunge  Controlled  Light 3 15  60Sec",  
-    }, {
-      Calories :"457",
-      text: "Upper Back 18,29 30-60 sec 1 1",
-    }, {
-      Calories :"457",
-      text: "Bike Fast  3min  Moderate  15  60Sec",      
-    },   
-  ];
+// detailrows = [
+//     {
+//       Name :"Jetts",
+//       slogan:"Enjoy your everyday with Jetts",  
+//       Open:"7x24 membership",
+//       Contact:"0800-0201-023",
+//       Location:"24 shally rd,Tamaki drive"
+//     }
+//   ];
 var DetailGymView = React.createClass({
   getInitialState: function(){
     _navigator = this.props.navigator;
-     var ds = new ListView.DataSource({rowHasChanged: (row1, row2) => true});
     this.state = {
-      day:this.props.date,
-      dataSource: ds.cloneWithRows(detailrows),
-      scrollEnabled: true,
+      name :"Jetts",
+      slogan:"Enjoy your everyday with Jetts",  
+      open:"7x24 membership",
+      contact:"0800-0201-023",
+      location:"24 shally rd,Tamaki drive"
     };
     return {
-      day:this.state.day,
-      dataSource: this.state.dataSource,
-      scrollEnabled: true,   
+       name:this.state.name,
+       slogan:this.state.slogan,
+       open:this.state.open,
+       contact:this.state.contact,
+       location:this.state.location
     };
   },
-  componentWillMount() {
-    let _that=this;
-    AsyncStorage.getItem('userid',(err, result) => {
-      console.log(result);
-      var trainee_id=result;
-      var day=this.props.date;
-      var ds = new ListView.DataSource({rowHasChanged: (row1, row2) => true});
-      var url = URLnetowrk+'detailplan.action';
-      // var url = 'http://192.168.20.12:8080/pt_server/traineelogin.action';
-      url += '?trainee_id='+trainee_id+'&day='+day;
-      console.log(url);
-      fetch(url).then(function(response) {  
-        return response.json();
-      }).then(function(res) {
-        console.log(res); 
-        if (res["data"]!=null) {        
-          _that.setState({
-            dataSource: ds.cloneWithRows(res["data"]),
-            detailrows:res["data"]
-          })
-        }else{
-          Alert.alert('Fail to display','Please check your data'); 
-        }  
-      });       
-    });  
-  },
+  // componentWillMount() {
+  //   let _that=this;
+  //   AsyncStorage.getItem('userid',(err, result) => {
+  //     console.log(result);
+  //     var trainee_id=result;
+  //     var day=this.props.date;
+  //     var ds = new ListView.DataSource({rowHasChanged: (row1, row2) => true});
+  //     var url = URLnetowrk+'detailplan.action';
+  //     // var url = 'http://192.168.20.12:8080/pt_server/traineelogin.action';
+  //     url += '?trainee_id='+trainee_id+'&day='+day;
+  //     console.log(url);
+  //     fetch(url).then(function(response) {  
+  //       return response.json();
+  //     }).then(function(res) {
+  //       console.log(res); 
+  //       if (res["data"]!=null) {        
+  //         _that.setState({
+  //           dataSource: ds.cloneWithRows(res["data"]),
+  //           detailrows:res["data"]
+  //         })
+  //       }else{
+  //         Alert.alert('Fail to display','Please check your data'); 
+  //       }  
+  //     });       
+  //   });  
+  // },
 //  set scrolling to true/false
-  allowScroll(scrollEnabled) {
-    this.setState({ scrollEnabled: scrollEnabled });
-  },
-  //  set active swipeout item
-  handleSwipeout(sectionID,rowID) {
-    for (var i = 0; i < this.state.detailrows.length; i++) {
+  // allowScroll(scrollEnabled) {
+  //   this.setState({ scrollEnabled: scrollEnabled });
+  // },
+  // //  set active swipeout item
+  // handleSwipeout(sectionID,rowID) {
+  //   for (var i = 0; i < this.state.detailrows.length; i++) {
       
-      if (i != rowID){
-        this.state.detailrows[i].active = false;
-      } 
-      else{
-        this.state.detailrows[i].active = true;
-      } 
-    }
-    this.updateDataSource(this.state.detailrows);
-  },
-  updateDataSource(data) {
-    this.setState({
-      dataSource: this.state.dataSource.cloneWithRows(data),
-    });
-  },
+  //     if (i != rowID){
+  //       this.state.detailrows[i].active = false;
+  //     } 
+  //     else{
+  //       this.state.detailrows[i].active = true;
+  //     } 
+  //   }
+  //   this.updateDataSource(this.state.detailrows);
+  // },
+  // updateDataSource(data) {
+  //   this.setState({
+  //     dataSource: this.state.dataSource.cloneWithRows(data),
+  //   });
+  // },
   delete:function(rowData){
     let _that=this;
     AsyncStorage.getItem('userid',(err, result) => {
@@ -175,36 +172,38 @@ var DetailGymView = React.createClass({
        });
     })
   },
-  renderRow(rowData: string, sectionID: number, rowID: number) {
-    var btnsTypes = [
-      { text: 'Edit', onPress: function(){ _navigator.push({
-                title:'EditplanView',
-                id:'editplan',
-                params:{date:rowData.day,
-                  itemname:rowData.item_name,
-                  dayplan_id:rowData.id
-                }
-              })},type: 'primary',},
-        { text: 'Submit',onPress:  () => { this.submitrecord(rowData) },type:'secondary'},
-        { text: 'Delete',onPress: () => { this.delete(rowData) },type: 'delete'},
-    ];
-    return (
-      <Swipeout
-        left={rowData.left}
-        right={btnsTypes}
-        rowID={rowID}
-        sectionID={sectionID}
-        autoClose={rowData.autoClose}
-        backgroundColor={rowData.backgroundColor}
-        close={!rowData.active}
-        onOpen={(sectionID, rowID) => this.handleSwipeout(sectionID, rowID) }
-        scroll={event => this.allowScroll(event)}>
-        <View style={styles.li}>
-              <Text style={styles.liText}>{rowData.item_name}Sportsize: {rowData.sportsize} </Text>        
-        </View>
-      </Swipeout>
-    );
-  },
+  // renderRow(rowData: string, sectionID: number, rowID: number) {
+  //   var btnsTypes = [
+  //     { text: 'Edit', onPress: function(){ _navigator.push({
+  //               title:'EditplanView',
+  //               id:'editplan',
+  //               params:{date:rowData.day,
+  //                 itemname:rowData.item_name,
+  //                 dayplan_id:rowData.id
+  //               }
+  //             })},type: 'primary',},
+  //       { text: 'Submit',onPress:  () => { this.submitrecord(rowData) },type:'secondary'},
+  //       { text: 'Delete',onPress: () => { this.delete(rowData) },type: 'delete'},
+  //   ];
+  //   return (
+  //     <Swipeout
+  //       left={rowData.left}
+  //       right={btnsTypes}
+  //       rowID={rowID}
+  //       sectionID={sectionID}
+  //       autoClose={rowData.autoClose}
+  //       backgroundColor={rowData.backgroundColor}
+  //       close={!rowData.active}
+  //       onOpen={(sectionID, rowID) => this.handleSwipeout(sectionID, rowID) }
+  //       scroll={event => this.allowScroll(event)}>
+  //       <View style={styles.li}>
+  //             <Text style={styles.liText}>Name:{rowData.Name}</Text>  
+  //             <Text style={styles.liText}>slogan: {rowData.slogan} </Text>
+  //             <Text style={styles.liText}>slogan: {rowData.slogan} </Text>           
+  //       </View>
+  //     </Swipeout>
+  //   );
+  // },
   render: function(){
     return(
        <ScrollView 
@@ -225,16 +224,28 @@ var DetailGymView = React.createClass({
               <View style={[styles.Topbar,styles.Right]}>             
             </View>
           </View>
-          <View style={[styles.header,styles.Bottomline]}>
-            <Image  source={require('../../img/plan_normal.png') }/>
-            <Text>{this.state.day} </Text>
+          <View style={styles.li}>
+            <View style={{flexDirection:'row'}}>
+              <Image source={require('../../img/gymname.png') }/>
+              <Text style={styles.liText}>  Name:{this.state.name}</Text> 
+            </View>
+            <View style={{flexDirection:'row'}}>  
+              <Image source={require('../../img/slogan.png') }/>   
+              <Text style={styles.liText}>  Slogan: {this.state.slogan} </Text>
+            </View>
+            <View style={{flexDirection:'row'}}>  
+              <Image source={require('../../img/phone.png') }/>
+              <Text style={styles.liText}>   Contact: {this.state.contact} </Text>
+            </View>
+            <View style={{flexDirection:'row'}}>  
+              <Image source={require('../../img/open.png') }/>
+              <Text style={styles.liText}>  open: {this.state.open} </Text>
+            </View>
+            <View style={{flexDirection:'row'}}>  
+              <Image source={require('../../img/location.png') }/>
+              <Text style={styles.liText}>  Location: {this.state.location} </Text>
+            </View>                
           </View>
-          <ListView style={styles.listview}
-            scrollEnabled={this.state.scrollEnabled}
-            dataSource={this.state.dataSource}
-            enableEmptySections={true}
-            renderRow={this.renderRow}
-            />
           <View>
             <BottomView {...this.props}/>
           </View>     
